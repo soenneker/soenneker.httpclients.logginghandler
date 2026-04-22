3d898c0b88d445c64fe697868610edb4f2e42e45
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-21 19:02:13 -0500

    TUnit conversion

--- test/Soenneker.HttpClients.LoggingHandler.Tests/HttpClientLoggingHandlerTests.cs
+++ test/Soenneker.HttpClients.LoggingHandler.Tests/HttpClientLoggingHandlerTests.cs
@@ -1,17 +1,16 @@
-﻿using Soenneker.Tests.FixturedUnit;
-using Xunit;
+using Soenneker.Tests.HostedUnit;
 
 namespace Soenneker.HttpClients.LoggingHandler.Tests;
 
-[Collection("Collection")]
-public sealed class HttpClientLoggingHandlerTests : FixturedUnitTest
+[ClassDataSource<Host>(Shared = SharedType.PerTestSession)]
+public sealed class HttpClientLoggingHandlerTests : HostedUnitTest
 {
 
-    public HttpClientLoggingHandlerTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
+    public HttpClientLoggingHandlerTests(Host host) : base(host)
     {
     }
 
-    [Fact]
+    [Test]
     public void Default()
     {
 
